dda132746c3a55be92afa0736bef4f445550729f
Mab-Nig <maivuphong4@gmail.com>
2024-08-28 18:38:30 +0700

    Configure Gradle for Firebase.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)

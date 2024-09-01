4922f2a2b8725722878fc500990490c7a1f2b7b5
PkKeo <minhcvn2011@gmail.com>
2024-09-01 12:27:27 +0700

    Home Record UI finish

--- app/src/main/java/project/cs426/hospitalbulance/HomeScreenHomeActivity.java
+++ app/src/main/java/project/cs426/hospitalbulance/HomeScreenHomeActivity.java
@@ -1,13 +1,17 @@
 package project.cs426.hospitalbulance;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class HomeScreenHomeActivity extends AppCompatActivity {
+public class HomeScreenHomeActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +19,21 @@ public class HomeScreenHomeActivity extends AppCompatActivity {
         Button cancel = findViewById(R.id.cancle_button);
         cancel.setTextColor(Color.parseColor("#FFFFFF"));
         cancel.setBackgroundColor(Color.parseColor("#C53434"));
+
+        ImageButton document = findViewById(R.id.document_button);
+        document.setOnClickListener(this);
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        if(v == findViewById(R.id.document_button))
+        {
+            String username = "Ronaldo";
+            Intent myIntent = new Intent(HomeScreenHomeActivity.this, HomeScreenRecordActivity.class);
+            myIntent.putExtra("username", username); //Optional parameters
+            this.startActivity(myIntent);
+        }
     }
 }

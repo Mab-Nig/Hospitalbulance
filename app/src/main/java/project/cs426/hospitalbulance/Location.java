2d9e11ba531e7759782e211e4ba5ace71811a9c9
PkKeo <minhcvn2011@gmail.com>
2024-09-05 18:29:40 +0700

    update_finding_placeID

---
+++ app/src/main/java/project/cs426/hospitalbulance/Location.java
@@ -0,0 +1,12 @@
+package project.cs426.hospitalbulance;
+
+public class Location {
+
+    public Location(double lat, double lng)
+    {
+        this.lat = lat;
+        this.lng = lng;
+    }
+    public double lat;
+    public double lng;
+}

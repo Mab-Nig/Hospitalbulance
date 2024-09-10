2af6a8621ae8368959b4ebc26c65b0a71c117272
Mab-Nig <maivuphong4@gmail.com>
2024-09-10 20:07:02 +0700

    Add Call object.

--- app/src/main/java/project/cs426/hospitalbulance/backend/database/Ambulance.java
+++ app/src/main/java/project/cs426/hospitalbulance/backend/database/Ambulance.java
@@ -3,16 +3,7 @@ package project.cs426.hospitalbulance.backend.database;
 import com.google.firebase.firestore.PropertyName;
 
 public class Ambulance {
-    private String number;
     private boolean isAvailable;
-
-    public String getNumber() {
-        return this.number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
 
     @PropertyName("is_available")
     public boolean isAvailable() {
